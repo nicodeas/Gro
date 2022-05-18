94e8eeb4be73708633b8344396f9b46b8b80bfd9
Nicodeas <nicholas.hy.michlin@gmail.com>
2022-05-18 22:34:46 +0800

    make password nullable = false

--- server/models.py
+++ server/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True,
                          unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128),nullable=False)
 
     moods = db.relationship('Mood', backref="user")
     journals = db.relationship("JournalEntry", backref="user")
