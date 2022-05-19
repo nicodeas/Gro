764dfb5cbf31944af57c03a1816476d57b18bfd1
Nicodeas <nicholas.hy.michlin@gmail.com>
2022-05-19 12:03:45 +0800

    add error messages to user signup login page

--- server/routes.py
+++ server/routes.py
@@ -1,5 +1,5 @@
 from server import app,db
-from flask import redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for
 from .models import User
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -30,7 +30,7 @@ def user_login_post():
     password = request.form.get('password')
     user = User.query.filter_by(username=username).first()
     if not user or not check_password_hash(user.password_hash,password):
-        # add error message here in future before re-rendering
+        flash('Please check login details')
         return redirect(url_for('user_login'))
     login_user(user)
     return redirect(url_for('index'))
@@ -48,7 +48,7 @@ def signup_user_post():
     last_name= request.form.get('last-name')
     user = User.query.filter_by(username=username).first()
     if user:
-        # add error message in future
+        flash('User already exists.')
         return redirect(url_for('signup_user'))
     new_user=User(first_name=first_name,last_name=last_name,username=username, password_hash=generate_password_hash(password,method='sha256'))
     db.session.add(new_user)
